e2bfa2f7bc33b5644576c759ff22b8e949cfee71
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-09-09 10:42:27 -0700

    no message

--- backend/src/client/services/client.service.ts
+++ backend/src/client/services/client.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PageOptionsDto } from '../../pagination/dtos/page-option.dto';
+import { PageDto } from '../../pagination/dtos/page.dto';
+import { PageMetaDto } from '../../pagination/dtos/page-meta.dto';
 import { ClientEntity } from '../entities/client.entity';
 import { Client } from '../entities/client.interface';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class ClientService {
@@ -11,15 +15,34 @@ export class ClientService {
     private clientRepository: Repository<ClientEntity>,
   ) {}
 
+  @Cron('*/15 * * * * *') //Runs every 15 seconds
   async findAll(): Promise<Client[]> {
+    let clients: Client[];
     const queryBuilder =  this.clientRepository
       .createQueryBuilder('FOREST_CLIENT')
       .where("FOREST_CLIENT.CLIENT_TYPE_CODE in ('C', 'A')")
-      .andWhere("FOREST_CLIENT.CLIENT_STATUS_CODE='ACT'")
-      .andWhere('FOREST_CLIENT.CORP_REGN_NMBR is not null')
-      .andWhere('FOREST_CLIENT.REGISTRY_COMPANY_TYPE_CODE is not null');
+      .andWhere("FOREST_CLIENT.CLIENT_STATUS_CODE='ACT'");
 
+    const itemCount = await queryBuilder.getCount();
+      
+    for (let i = 1; i <= 5; i++ ) {
+      
+      const take = 10;
+      const skip = (i - 1) * take;
+      
+      queryBuilder.skip(skip).take(take).getMany()
+      .then((res) => {
+        console.log("Page #: " + i);
+        if (res) {
+          console.log(res);
+        }
+      });
+      //clients.push(queryBuilder.skip(skip).take(take).getMany());
+    }
+    //console.log(itemCount);
+    
     return queryBuilder.take(10).getMany();
+    //return clients;
   }
 
 }
