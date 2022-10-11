a2d016bc21f7d610de80fa36bf566293fd33f7e9
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-10-11 11:31:43 -0700

    Cleaned up POM #1

--- src/main/java/ca/bc/gov/app/core/controller/CoreController.java
+++ src/main/java/ca/bc/gov/app/core/controller/CoreController.java
@@ -4,9 +4,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @RestController
 public class CoreController {
 
