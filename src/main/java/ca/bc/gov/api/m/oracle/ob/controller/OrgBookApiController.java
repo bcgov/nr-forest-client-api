952b46aa06ddfd0712d823363deffd089914962a
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-10-26 17:18:46 -0700

    feat/javaapi: Added example to access external API

---
+++ src/main/java/ca/bc/gov/api/m/oracle/ob/controller/OrgBookApiController.java
@@ -0,0 +1,36 @@
+package ca.bc.gov.api.m.oracle.ob.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.bc.gov.api.core.configuration.OraclePersistenceConfiguration;
+import ca.bc.gov.api.m.oracle.ob.service.OrgBookApiService;
+import io.swagger.annotations.Api;
+
+@Api(tags = OraclePersistenceConfiguration.ORACLE_API_TAG)
+@RestController
+@RequestMapping("api/m/orgbook/")
+public class OrgBookApiController {
+
+    public static final Logger logger = LoggerFactory.getLogger(OrgBookApiController.class);
+    
+    @Inject
+    private OrgBookApiService orgBookApiService;
+
+    @RequestMapping(value = "/findByIncorporationNumber", 
+                    method = RequestMethod.GET, 
+                    produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> findByIncorporationNumber(@RequestParam("incorporationNumber") String incorporationNumber) {
+        return orgBookApiService.findByIncorporationNumber(incorporationNumber);
+    }
+
+}
