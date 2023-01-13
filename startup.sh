80985746ed4907b2da9f6fd5ccae7b8a03fb3da3
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-01-13 13:34:36 -0800

    fix: fixing image startup

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@ java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
 keytool -exportcert -alias "${ORACLEDB_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
 keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
 
-java -cp /app:/app/lib/* ca.bc.gov.api.BootApplication
+java -cp /app:/app/lib/* ca.bc.gov.api.oracle.legacy.Application
 
