07cc915dab2b788f0bfead3045d1abaee73b3bef
brunoMarchiEncora <bruno.marchi@encora.com>
2023-01-24 13:41:24 -0300

    Improved tests and fixed issue with querydsl

---
+++ src/test/java/ca/bc/gov/api/oracle/legacy/AbstractTestContainerIntegrationTest.java
@@ -0,0 +1,65 @@
+package ca.bc.gov.api.oracle.legacy;
+
+import java.util.UUID;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ExtendWith({SpringExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
+public abstract class AbstractTestContainerIntegrationTest {
+
+  static final OracleContainer database;
+
+  static {
+    database = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+        .withDatabaseName("legacyfsa")
+        .withUsername("THE")
+        .withPassword(UUID.randomUUID().toString().substring(24));
+    database.start();
+  }
+
+  @DynamicPropertySource
+  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.database",
+            database::getDatabaseName);
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.service",
+            database::getDatabaseName);
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.host",
+            () -> String.format("%s:%d",
+                database.getHost(),
+                database.getMappedPort(1521)));
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.username",
+            database::getUsername);
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.schema",
+            database::getUsername
+        );
+
+    registry
+        .add(
+            "ca.bc.gov.nrs.oracle.password",
+            database::getPassword);
+  }
+}
