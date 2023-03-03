d657b41cd1fe4fec3abf31ae77d76c52726fd068
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-03-03 07:55:28 -0800

    chore: removing unused files

--- api/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
-keytool -exportcert -alias "${ORACLEDB_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
-keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
-
-java -Djava.security.egd=file:/dev/./urandom -jar /app/service.jar
-
