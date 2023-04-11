967843f5c6b8c37e927e358b3d4f67114d277c21
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-04-11 15:37:09 -0700

    feat(FSADT1-739): certificate volume claim (#149)
    * feat: certificate volume claim
    
    adding volume claim to certificate to allow for faster deployments
    
    * fix: fixing name
    
    * chore: updating init container for testing
    
    * fix(deps): update dependency org.springframework.boot:spring-boot-starter-parent to v3.0.5
    
    * chore: updating init container for testing
    
    * chore: updating init container for testing
    
    * feat: moving init from java to shell
    
    * chore: adding cert secret
    
    * fix: setting a version for the dockerfile
    
    ---------
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM eclipse-temurin:17.0.6_10-jdk-alpine@sha256:a765a97826df90554f3d3a98be5586012bbc53593876f669ff4b2e68717be71d
-
-WORKDIR /app
+FROM alpine:3.17.3@sha256:b6ca290b6b4cdcca5b3db3ffa338ee0285c11744b4a6abaa9627746ee3291d8d
 
 ENV LANG en_CA.UTF-8
 ENV LANGUAGE en_CA.UTF-8
 ENV LC_ALL en_CA.UTF-8
 
-COPY startup.sh .
-COPY InstallCert.java .
+RUN apk --no-cache add openssl openjdk8
 
-RUN chmod g+w /app && \
-    chmod g+x startup.sh && \
-    chmod g+w ${JAVA_HOME}/lib/security/cacerts
+RUN which openssl
+RUN which keytool
+
+COPY startup.sh .
+
+RUN chmod g+x startup.sh
 
 # Non-privileged user
 USER app
--- startup.sh
+++ startup.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
-mkdir -p /cert/certs
-rm -rf /cert/certs/*
+cert_folder="/cert"
+cert_file="jssecacerts"
 
-echo "I will try to get the ${ORACLEDB_HOST}-1 cert"
-echo "Connecting to ${ORACLEDB_HOST}:${ORACLEDB_PORT}"
-java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
+mkdir -p $cert_folder
 
-cp jssecacerts /cert/jssecacerts
+generate_cert() {
+
+  echo "I will try to get the ${ORACLEDB_HOST}-1 cert"
+  echo "Connecting to ${ORACLEDB_HOST}:${ORACLEDB_PORT}"
+
+  openssl s_client -connect ${ORACLEDB_HOST}:${ORACLEDB_PORT} -showcerts </dev/null | openssl x509 -outform pem > $cert_folder/${ORACLEDB_HOST}.pem
+  openssl x509 -outform der -in $cert_folder/${ORACLEDB_HOST}.pem -out $cert_folder/${ORACLEDB_HOST}.der
+  keytool -import -alias ${ORACLEDB_HOST} -keystore $cert_file -file $cert_folder/${ORACLEDB_HOST}.der -storepass ${ORACLEDB_SECRET} -noprompt
+
+  cp $cert_file $cert_folder/$cert_file
+  echo "Generated $cert_file and copied it to $cert_folder."
+}
+
+if [ "$(ls -A $cert_folder)" ]; then
+  echo "The $cert_folder folder is not empty."
+  if [ -e "$cert_folder/$cert_file" ]; then
+    echo "The "$cert_folder/$cert_file" certificate file is present."
+  else
+    generate_cert
+  fi
+else
+  generate_cert
+fi
+
