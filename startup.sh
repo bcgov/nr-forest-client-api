fd0242d1072fee20ddd217491414c4e0a1530226
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-04-11 09:26:46 -0700

    feat: certificate volume claim
    adding volume claim to certificate to allow for faster deployments

--- startup.sh
+++ startup.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-mkdir -p /cert/certs
-rm -rf /cert/certs/*
+cert_folder="/cert"
+cert_file="jssecacerts"
 
-echo "I will try to get the ${ORACLEDB_HOST}-1 cert"
-echo "Connecting to ${ORACLEDB_HOST}:${ORACLEDB_PORT}"
-java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
+mkdir -p $cert_folder
 
-cp jssecacerts /cert/jssecacerts
+generate_cert() {
+  echo "I will try to get the ${ORACLEDB_HOST}-1 cert"
+  echo "Connecting to ${ORACLEDB_HOST}:${ORACLEDB_PORT}"
+  java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
+  cp "$cert_file" "$cert_folder/$cert_file"
+  echo "Generated $cert_file and copied it to $cert_folder."
+}
+
+if [ "$(ls -A $cert_folder)" ]; then
+  echo "The $cert_folder folder is not empty."
+  if [ -e "$cert_file" ]; then
+    echo "The $cert_file certificate file is present."
+  else
+    generate_cert
+  fi
+else
+  generate_cert
+fi
