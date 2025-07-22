b5741b3e31bf847927ff07d06d8ce67d4bb17708
Maria Martinez <maria.martinez@gov.bc.ca>
2025-07-21 19:51:39 -0700

    chore: Fixed vulnerability CVE-2025-22227

--- src/main/java/ca/bc/gov/api/oracle/legacy/Application.java
+++ src/main/java/ca/bc/gov/api/oracle/legacy/Application.java
@@ -1,11 +1,13 @@
 package ca.bc.gov.api.oracle.legacy;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.LoopResources;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
@@ -20,5 +22,15 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+    
+    LoopResources loopResources = LoopResources.create("http", 4, 8, true);
+
+    HttpServer server = HttpServer.create()
+        .runOn(loopResources, false)
+        .port(8080);
+
+    server.bindNow()
+          .onDispose()
+          .block();
   }
 }
