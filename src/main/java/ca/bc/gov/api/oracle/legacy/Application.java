09565589c2e4b01f852334e359ad74da59dd320f
Maria Martinez <maria.martinez@gov.bc.ca>
2025-07-21 21:49:36 -0700

    no message

--- src/main/java/ca/bc/gov/api/oracle/legacy/Application.java
+++ src/main/java/ca/bc/gov/api/oracle/legacy/Application.java
@@ -1,11 +1,11 @@
 package ca.bc.gov.api.oracle.legacy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
