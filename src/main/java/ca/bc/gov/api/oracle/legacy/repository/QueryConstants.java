d650f3b507f0de90606589488d239e0596325bd5
Maria Martinez <maria.martinez@gov.bc.ca>
2026-06-25 13:05:51 -0700

    no message

--- src/main/java/ca/bc/gov/api/oracle/legacy/repository/QueryConstants.java
+++ src/main/java/ca/bc/gov/api/oracle/legacy/repository/QueryConstants.java
@@ -14,17 +14,21 @@ public class QueryConstants {
           CASE WHEN CLIENT_ACRONYM = :acronym THEN 800 ELSE 0 END +
           CASE WHEN CLIENT_NUMBER = :clientNumber THEN 1000 ELSE 0 END +
           UTL_MATCH.JARO_WINKLER_SIMILARITY(
-            TRIM(COALESCE(LEGAL_FIRST_NAME || ' ', ''))
-              || TRIM(COALESCE(LEGAL_MIDDLE_NAME || ' ', ''))
-              || COALESCE(CLIENT_NAME, ''),
+            TRIM(
+              COALESCE(LEGAL_FIRST_NAME || ' ', '')
+                || COALESCE(LEGAL_MIDDLE_NAME || ' ', '')
+                || COALESCE(CLIENT_NAME, '')
+            ),
             :clientName)
         ) AS score
       FROM THE.FOREST_CLIENT
       WHERE
         UTL_MATCH.JARO_WINKLER_SIMILARITY(
-          TRIM(COALESCE(LEGAL_FIRST_NAME || ' ', ''))
-            || TRIM(COALESCE(LEGAL_MIDDLE_NAME || ' ', ''))
-            || COALESCE(CLIENT_NAME, ''),
+          TRIM(
+            COALESCE(LEGAL_FIRST_NAME || ' ', '')
+              || COALESCE(LEGAL_MIDDLE_NAME || ' ', '')
+              || COALESCE(CLIENT_NAME, '')
+          ),
           :clientName) >= 80
         OR CLIENT_ACRONYM = :acronym
         OR CLIENT_NUMBER = :clientNumber
@@ -42,9 +46,11 @@ public class QueryConstants {
           (
             NVL(:clientName, 'NOVALUE') = 'NOVALUE'
             OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-              TRIM(COALESCE(LEGAL_FIRST_NAME || ' ', ''))
-                || TRIM(COALESCE(LEGAL_MIDDLE_NAME || ' ', ''))
-                || COALESCE(CLIENT_NAME, ''),
+              TRIM(
+                COALESCE(LEGAL_FIRST_NAME || ' ', '')
+                  || COALESCE(LEGAL_MIDDLE_NAME || ' ', '')
+                  || COALESCE(CLIENT_NAME, '')
+              ),
               :clientName) >= 80
           )
       )
