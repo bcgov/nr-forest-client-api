8b13e85b5baba30bb06b1f880b0f2daf20e5eaad
Maria Martinez <maria.martinez@gov.bc.ca>
2026-07-16 12:54:02 -0700

    chore: upgraded to spring 4

--- src/main/java/ca/bc/gov/api/oracle/legacy/configuration/MetricConfiguration.java
+++ src/main/java/ca/bc/gov/api/oracle/legacy/configuration/MetricConfiguration.java
@@ -6,7 +6,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
